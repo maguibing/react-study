d03e8367d8c664e2aca2ffce5a0cdb519cd582b5
maguibing <kuse012@163.com>
2022-05-31 22:48:35 +0800

    react  props  函数式组件  基本使用

--- react-第三天/src/App.js
+++ react-第三天/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDom from 'react-dom/client'
 
-function App() {
+// 函数组件的基本使用
+export default function Son(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {props.name} {'123'}
     </div>
-  );
+  )
 }
-
-export default App;
