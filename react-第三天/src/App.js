314fc40c53de2dcb0ac78acea1a9b3acc74491a3
maguibing <kuse012@163.com>
2022-06-01 22:33:39 +0800

    基础 父传子 组件传值  循环组件

--- react-第三天/src/App.js
+++ react-第三天/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react'
 
-// 函数组件的基本使用
-export default function Son(props) {
-  return (
-    <React.Fragment>
-      <div>
-        {props.name} {'123'}
-      </div>
-      <hr />
-      <Child name="劳务" age="12312"></Child>
-    </React.Fragment>
-  )
-}
-
-class Child extends React.Component {
+export default class Child extends React.Component {
+  state = {
+    news: [{ a: 1 }, { a: 2 }, { a: 3 }],
+  }
   render() {
+    const { news } = this.state
     return (
       <React.Fragment>
-        <span>{this.props.name}</span>
+        {news.map((v, i) => (
+          <Parent key={i} />
+        ))}
       </React.Fragment>
     )
   }
+}
+
+function Parent() {
+  return (
+    <React.Fragment>
+      <div className="parent">
+        <h1>今日要闻</h1>
+        <div className="child">
+          <h3>坚定不移走中国特色社会主义法治道路</h3>
+          <div className="detail">
+            <span>新华社</span>
+            <span>774点赞</span>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
