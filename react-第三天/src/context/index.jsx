3ab168d5babb76eff34bdb9c833897372b52e023
maguibing <kuse012@163.com>
2022-06-11 21:29:18 +0800

    评论组件拆分

--- react-第三天/src/context/index.jsx
+++ react-第三天/src/context/index.jsx
@@ -1,55 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import CommentHead from './components/CommentHead'
+import CommentInput from './components/CommentInput'
+import CommentList from './components/CommentList'
+// import './index.css'
 
-const hisContext = React.createContext(0)
-
-
-export default class Parent extends React.Component { 
-    state = {
-        money:1000   
-    }
-
-    updateMoeny (value) { 
-        this.setState({
-            money: value
-        })
-    }
-
-    render () { 
-        return (
-        <React.Fragment>
-                    <h1>关键内容{ this.state.money}</h1>
-            <hisContext.Provider value={{money:this.state.money, updateMoeny:this.updateMoeny.bind(this)}}>
-                <Son></Son>
-            </hisContext.Provider>
-        </React.Fragment>
-        )
-    }
-}
-
-class Son extends React.Component{
-    render () { 
-        return (
-            <div>
-                <h1>儿子</h1>
-                <GrandSon></GrandSon>
-            </div>
-        )
-    }
-}
-
-class GrandSon extends React.Component{
-    render () { 
-        return (
-            <hisContext.Consumer>
-            {(value) => 
-            (
-                <div>
-                    < h1 > 孙子:{value.money}</h1>
-                    <button onClick={()=>value.updateMoeny(500)}> 修改金额 </button>
-               </div>
-            )
-                   }
-           </hisContext.Consumer>
-        )
-    }
+export default class App extends Component {
+  render() {
+    return (
+      <div className="comments">
+        {/* 输入框组件 */}
+        <CommentInput />
+        {/* 标题排序组件 */}
+        <CommentHead />
+        {/* 列表组件 */}
+        <CommentList />
+      </div>
+    )
+  }
 }
