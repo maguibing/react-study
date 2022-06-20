365db96f563d49f3fa82b92abb94304f6d39c227
maguibing <kuse012@163.com>
2022-06-20 20:42:04 +0800

    todos基础结构

--- react-第四天/src/index.js
+++ react-第四天/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App array={[1, 2, 3]} />
+    <App />
   </React.StrictMode>,
 )
 
