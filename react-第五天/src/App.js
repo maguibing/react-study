d9482c4ab6978c44dd0d242c63c0a14df0bf42ad
maguibing <kuse012@163.com>
2022-06-21 21:53:19 +0800

    react-第五天 init

---
+++ react-第五天/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+/**
+ * this.setState((prev)=>{return {}})
+ * this.setState((prev)=>{return {}},()=>{更新后的值})
+ * setTimeout(() => {
+    this.setState({count: count+1})
+ }, timeout);
+ * @returns
+ */
+
+function App() {
+  return <div className="App">123</div>
+}
+
+export default App
